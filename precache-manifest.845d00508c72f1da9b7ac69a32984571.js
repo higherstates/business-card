bb31335c01faff20e645a9f142fca28a17173ef5
higherstates <thanhthuyh2007@gmail.com>
2021-12-09 15:47:55 +0700

    Updates

---
+++ precache-manifest.845d00508c72f1da9b7ac69a32984571.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d6b2b5461cb0fff57ed900191392d0",
+    "url": "/business-card/index.html"
+  },
+  {
+    "revision": "9ccb44430edce109f46f",
+    "url": "/business-card/static/css/main.0e34871d.chunk.css"
+  },
+  {
+    "revision": "e6ecbb7779fc1f2b9bc8",
+    "url": "/business-card/static/js/2.74f7727a.chunk.js"
+  },
+  {
+    "revision": "337690bdb71522eaada8e2b2e5b3e79a",
+    "url": "/business-card/static/js/2.74f7727a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccb44430edce109f46f",
+    "url": "/business-card/static/js/main.7f2f10fe.chunk.js"
+  },
+  {
+    "revision": "7dafc5f322ed3e0831cd",
+    "url": "/business-card/static/js/runtime-main.0438bdc9.js"
+  },
+  {
+    "revision": "58eff5d8e6a4f2d19c464f4b1acf5ede",
+    "url": "/business-card/static/media/profile.58eff5d8.jpg"
+  }
+]);
